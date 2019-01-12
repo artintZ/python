43d71527fc655098a8b046c66e6f174d354012d9
zzk <707381369@qq.com>
2019-01-12 23:01:18 +0800

    变量的调用

---
+++ day1/var_print_info.py
@@ -0,0 +1,36 @@
+#打印多行字符串,亦作多行注释
+msg = '''
+    name = "zzk"
+    age = 22
+    job = "student"
+    '''
+print(msg)
+
+#用户输入input()默认字符串类型
+name = input("name =")
+age = int(input("age ="))   #函数int()转换为整型
+print("name =", name, age)    #逗号可以改成加号
+
+#多行字符串变量调用，方法一
+info = '''
+    ---info of %s---
+    name = %s
+    age = %d
+    ''' %(name, name, age)
+print(info)
+
+#多行字符串变量调用，方法二
+info = '''
+    ---info of {_name}---
+    name = {_name}
+    age = {_age}
+    ''' .format(_name = name, _age = age)
+print(info)
+
+#多行字符串变量调用，方法三
+info = '''
+    ---info of {0}---
+    name = {0}
+    age = {1}
+    ''' .format(name, age)
+print(info)
