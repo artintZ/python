01001f08dd3ad712f49e81abcdcb4e6a98463430
zzk <707381369@qq.com>
2019-01-14 22:22:53 +0800

    注释格式的修改

--- day1/var_print_info.py
+++ day1/var_print_info.py
@@ -1,4 +1,7 @@
-#打印多行字符串,亦作多行注释
+# Author: zzk
+
+
+# 打印多行字符串,亦作多行注释
 msg = '''
     name = "zzk"
     age = 22
@@ -6,12 +9,12 @@ msg = '''
     '''
 print(msg)
 
-#用户输入input()默认字符串类型
+# 用户输入input()默认字符串类型
 name = input("name =")
-age = int(input("age ="))   #函数int()转换为整型
-print("name =", name, age)    #逗号可以改成加号
+age = int(input("age ="))   # 函数int()转换为整型
+print("name =", name, age)    # 逗号可以改成加号
 
-#多行字符串变量调用，方法一
+# 多行字符串变量调用，方法一
 info = '''
     ---info of %s---
     name = %s
@@ -19,7 +22,7 @@ info = '''
     ''' %(name, name, age)
 print(info)
 
-#多行字符串变量调用，方法二
+# 多行字符串变量调用，方法二
 info = '''
     ---info of {_name}---
     name = {_name}
@@ -27,7 +30,7 @@ info = '''
     ''' .format(_name = name, _age = age)
 print(info)
 
-#多行字符串变量调用，方法三
+# 多行字符串变量调用，方法三
 info = '''
     ---info of {0}---
     name = {0}
