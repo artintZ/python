4a39dfddc4382407d52b58e57cc62aad43f748d4
zzk <707381369@qq.com>
2019-02-09 22:55:47 +0800

    list购物车程序

--- day2/shopping.py
+++ day2/shopping.py
@@ -2,10 +2,11 @@
 
 goods = (('手机', 3000), ('电脑', 8000), ('汽车', 150000),
          ('餐具', 300), ('家具', 20000), ('家电', 30000))
-goods_sold = []
+goods_bought = []  # 已购买商品清单
 SALARY = int(input('请输入你的工资:'))
 BALANCE = SALARY
 
+"""
 def menu():
     print('\n编号','商品',' 价格')
     for i in range(goods.__len__()):
@@ -15,21 +16,30 @@ def menu():
     print('\n 0 、退出')
     n = int(input('请输入想要购买的商品的编号:'))
     return n
+"""
+
+def menu():
+    print('\n编号','商品','价格')
+    for i,(j, k) in enumerate(goods):
+        print(i+1, j, k)
+    print('0 退出')
+    n = int(input('请输入想要购买的商品的编号:'))
+    return n
 
 def buy(n):
     global BALANCE
-    if goods[n - 1][1] <= BALANCE:
-        BALANCE = BALANCE - goods[n - 1][1]
-        goods_sold.append(goods[n - 1])
+    if goods[n-1][1] <= BALANCE:
+        BALANCE = BALANCE - goods[n-1][1]
+        goods_bought.append(goods[n-1])
         print('购买成功！花了{}元，余额为{}元'.format(goods[n-1][1], BALANCE))
     else:
         print('余额不足！价格为{}元，余额为{}元'.format(goods[n-1][1], BALANCE))
-    input()
+    input('继续')
 
 def balance():
     print('\n购物清单：')
-    for i,(j,k) in enumerate(goods_sold):
-        print(i, j, k)
+    for i,(j,k) in enumerate(goods_bought):
+        print(i+1, j, k)
     print('您一共消费{}元，您的余额{}元'.format(SALARY - BALANCE, BALANCE))
     input('按任意键退出')
 
@@ -40,7 +50,3 @@ while 1:
     else:
         break
 balance()
-    
-
-
-
