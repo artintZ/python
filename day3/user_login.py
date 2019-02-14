069e814a42aafcad95c67b07b8ff95b6e12d9bcf
zzk <707381369@qq.com>
2019-02-15 01:53:21 +0800

    用户名和密码优化算法

--- day3/user_login.py
+++ day3/user_login.py
@@ -1,8 +1,19 @@
 # Author: zzk
 
-import os
+#import os
 
-with open('future', 'r') as f:
-    for line in f:
-        if 'hello'+': '+'name\n' in line:
-            print('welcome')
+
+def login():
+    for i in range(3):
+        usrname = input('请输入用户名：')
+        passwd = input('请输入密码：')
+        id_string = '#'.join(['', usrname, passwd, ''])
+        if id_string == '#admin#admin#':
+            return 1
+        with open('future', 'r') as f:
+            for line in f:
+                if id_string in line:
+                    print('welcome %s', usrname)
+
+# 判断嵌套，依次判断用户名、密码
+login()
