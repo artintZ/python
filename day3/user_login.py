6657c88ad2cd05e13a5d794c686bfb8e97c6595e
zzk <707381369@qq.com>
2019-02-16 19:00:08 +0800

    完成用户登录程序

--- day3/user_login.py
+++ day3/user_login.py
@@ -15,7 +15,10 @@ def lock(usrname, tag_old='#', tag_new='$'):
             f_new.write(line)
     os.remove('id_data')
     os.rename('id_data.bak', 'id_data')
-# lock('zzk','zzk')
+    if tag_old == '#':
+        print('用户已被锁定，请联系管理员解锁！\n')
+    else:
+        print('用户已解锁，可以正常登录！\n')
 
 
 def unlock(usrname):
@@ -23,36 +26,17 @@ def unlock(usrname):
     lock(usrname, tag_old='$', tag_new='#')
 
 
-# unlock('zzk', 'zzk')
-
-def islocked(usrname):
-    '''判断用户是否锁定，若锁定则返回1，否则返回0'''
+def isstatus(usrname):
+    '''判断用户状态，若已注册且状态正常则返回1，\
+        已注册并且被锁定返回2，未被注册返回0'''
     with open('id_data', 'r', encoding='utf-8') as f:
         for line in f:
-            if usrname.join(['$', '$']) in line:
+            if usrname.join(['#', '#']) in line:
                 return 1
+            elif usrname.join(['$', '$']) in line:
+                return 2
         else:
             return 0
-# unlock('zzk')
-# print(islocked('zzk'))
-
-
-def username():
-    '''返回未被注册的用户名'''
-    with open('id_data', 'r', encoding='utf-8') as f:
-        while True:
-            usrname = input('请输入用户名：')
-            if usrname == 'b':
-                break
-            else:
-                for line in f:
-                    if usrname.join(['#', '#']) in line\
-                        or usrname.join(['$', '$']) in line:
-                        print('用户名已被注册，请重新输入！\n')
-                        break
-                else:
-                    return usrname
-    
 
 
 def iscorrect(usrname, passwd):
@@ -69,35 +53,45 @@ def iscorrect(usrname, passwd):
 def register():
     '''注册，在文件中添加用户名和密码'''
     while True:
+        print('\n\n', '注册/按b返回'.center(20, '-'))
         usrname = input('请设置用户名：')
         if usrname == 'b':
             break
-        elif username(usrname):
-            print('用户名已被注册，请重新输入！')
+        elif len(usrname) < 3:
+            print('用户名至少三个字符,请重新输入！\n')
+        elif isstatus(usrname):
+            print('用户名已被注册，请重新输入！\n')
         else:
-            passwd = input('请设置密码：')
+            passwd = input('请设置密码/按b返回：')
+            if passwd == 'b':
+                break
             with open('id_data', 'a', encoding='utf-8') as f:
                 id_normal = '#'.join(['', usrname, passwd, '\n'])
                 f.write(id_normal)
             print('\n注册成功！返回主菜单……\n\n')
             break
 
-# register('a', 'b')
 
-
-def delete(usrname):
-    '''删除文件中的用户名和密码'''
-    with open('id_data', 'r', encoding='utf-8') as f,\
-            open('id_data.bak', 'w', encoding='utf-8') as f_new:
-        for line in f:
-            if usrname.join(['#', '#']) in line\
-                    or usrname.join(['$', '$']) in line:
-                continue
-            f_new.write(line)
-    os.remove('id_data')
-    os.rename('id_data.bak', 'id_data')
-
-# delete('hello')
+def delete():
+    '''删除文件中的用户名和密码：新建文件修改，再删除原文件并重命名新文件'''
+    while True:
+        print('\n\n', '删除/按b返回'.center(20, '-'))
+        usrname = input('请输入用户名：')
+        if usrname == 'b':
+            break
+        elif not isstatus(usrname):
+            print('用户名不存在，请重新输入！\n')
+        else:
+            with open('id_data', 'r', encoding='utf-8') as f,\
+                    open('id_data.bak', 'w', encoding='utf-8') as f_new:
+                for line in f:
+                    if usrname.join(['#', '#']) in line\
+                            or usrname.join(['$', '$']) in line:
+                        continue
+                    f_new.write(line)
+            os.remove('id_data')
+            os.rename('id_data.bak', 'id_data')
+            print('删除成功\n')
 
 
 def admin_menu():
@@ -121,17 +115,84 @@ def administrator():
     while True:
         choice = admin_menu()
         if choice == 1:
-            print('\n\n', '管理员添加/按b返回'.center(20, '-'))
             register()
         elif choice == 2:
-            print('\n\n', '管理员删除/按b返回'.center(20, '-'))
             delete()
+        elif choice == 3:
+            while True:
+                print('\n\n', '解锁/按b返回'.center(20, '-'))
+                usrname = input('请输入用户名')
+                if usrname == 'b':
+                    break
+                elif isstatus(usrname) == 2:
+                    unlock(usrname)
+                else:
+                    print('该用户无需解锁！\n')
+                    break
+        elif choice == 4:
+            while True:
+                print('\n\n', '上锁/按b返回'.center(20, '-'))
+                usrname = input('请输入用户名')
+                if usrname == 'b':
+                    break
+                elif isstatus(usrname) == 1:
+                    lock(usrname)
+                else:
+                    print('该用户无需上锁！\n')
+                    break
+        elif choice == 5:
+            while True:
+                print('\n\n', '查询/按b返回'.center(20, '-'))
+                usrname = input('请输入用户名：')
+                if usrname == 'b':
+                    break
+                elif isstatus(usrname) == 1:
+                    print('该用户状态正常。\n')
+                elif isstatus(usrname) == 2:
+                    print('该用户已被锁定。\n')
+                else:
+                    print('没有找到该用户！\n')
+        else:
+            exit()
+
+
+def login():
+    '''登录过程，用户和管理员登录'''
+    while True:
+        print('\n\n', '登录界面/按b返回'.center(20, '-'))
+        usrname = input('请输入用户名：')
+        if usrname == 'b':
+            break
+        elif usrname == 'admin':
+            for cnt in range(3):
+                passwd = input('请输入管理员密码：')
+                if passwd == 'admin':
+                    administrator()
+                else:
+                    print('密码错误 %d 次，错误三次返回主菜单！\n' % (cnt+1))
+            else:
+                break
+        elif isstatus(usrname) == 1:
+            for cnt in range(3):
+                passwd = input('请输入密码/按b返回：')
+                if passwd == 'b':
+                    break
+                elif iscorrect(usrname, passwd):
+                    exit('登录成功！正在退出……')
+                else:
+                    print('密码错误 %d 次，错误三次锁定用户！\n' % (cnt+1))
+            else:
+                lock(usrname)
+        elif isstatus(usrname) == 2:
+            print('用户已被锁定，请联系管理员解锁！\n')
+        else:
+            print('用户名不存在，请先注册！\n')
 
 
 def main_menu():
     """主菜单，返回值'1. 登录', '2. 注册', '0. 退出'"""
     menu = ('1. 登录', '2. 注册', '0. 退出')
-    print('主界面'.center(20, '-'))
+    print('\n\n', '主界面'.center(20, '-'))
     for i in menu:
         print(i)
     while True:
@@ -141,46 +202,13 @@ def main_menu():
         else:
             print('输入不正确，请重新输入！\n')
 
-# print('0' in ['2'] or '2' in ['0'])
-
 
 # 主函数
 while True:
     choice = main_menu()
     if choice == 1:
-        print('\n\n', '登录界面'.center(20, '-'))
-        usrname = input('请输入用户名：')
-        if usrname == 'admin':
-            for cnt in range(3):
-                passwd = input('请输入管理员密码：')
-                if passwd == 'admin':
-                    while True:
-                        choice = admin_menu()
-                        '''if choice == 1:
-                            print('\n\n', '管理员添加界面/按b返回'.center(20, '-'))
-                            register()'''
-
-                else:
-                    print('密码错误 %d 次，错误三次返回主菜单！\n' % (cnt+1))
-        elif not username(usrname):
-            print('用户名不存在，请先注册！\n')
-        elif islocked(usrname):
-            print('用户已被锁定，请联系管理员解锁！\n')
-        else:
-            for cnt in range(3):
-                passwd = input('请输入密码：')
-                if iscorrect(usrname, passwd):
-                    print('登录成功！正在退出……')
-                    exit()
-                else:
-                    print('密码错误 %d 次，错误三次锁定用户！' % (cnt+1))
-            else:
-                lock(usrname)
-                print('用户已被锁定，请联系管理员解锁！\n')
+        login()
     elif choice == 2:
-        print('\n\n', '注册界面/按b返回'.center(20, '-'))
         register()
     else:
         exit()
-
-# 优化判断用户名是否存在和锁定！！！！！！！！！！！！！
