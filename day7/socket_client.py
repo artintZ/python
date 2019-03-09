22fc8444d585526e0e38166725f38f8479da9267
zzk <707381369@qq.com>
2019-03-09 18:43:10 +0800

    socket通讯

--- day7/socket_client.py
+++ day7/socket_client.py
@@ -3,10 +3,13 @@
 
 import socket
 
-client = socket.socket()
-client.connect(('localhost', 6969))
-
-client.send(b'Hello world!')
-data = client.recv(1024)
-print(data)
+client = socket.socket()  # 声明socket类型，同时生成socket连接对象
+client.connect(('127.0.0.1', 6969))
+while True:
+    msg = input().strip()
+    if len(msg) == 0:
+        continue
+    client.send(b'client:' + msg.encode())
+    data = client.recv(10240)
+    print(data.decode())
 client.close()
--- day7/socket_sever.py
+++ day7/socket_sever.py
@@ -5,14 +5,24 @@ import socket
 
 sever = socket.socket()
 sever.bind(('localhost', 6969))  # 绑定要监听端口
-sever.listen()  # 监听
-
-conn, addr = sever.accept()
-
-data = conn.recv(1024)
-
-print(data)
-conn.send(data.upper())
+sever.listen(5)  # 监听
+print('服务器已开启')
+while True:
+    conn, addr = sever.accept()
+    print(conn, addr)
+    # print('电话来了')
+    cnt = 0
+    while True:
+        data = conn.recv(1024)
+        print(data.decode())
+        if not data:
+            # continue
+            print('Client has lost...')
+            break
+        msg = input().strip()
+        conn.send(b'sever:' + msg.encode())
+        cnt += 1
+        if cnt > 9:
+            break
 
 sever.close()
-
